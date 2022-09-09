240a77f5861efa6a0ea8c9e2a67f21e8d0c78dc8
Selim Mammadov <mamedovfhn@gmail.com>
2022-09-09 10:44:27 +0400

    task-3

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,25 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+    let scores = result.input
+    let bad = 0;
+    let good = 0
+    for (let i = 0; i < scores.length; i++) {
+        const element = scores[i];
+        if (element == 2) {
+            bad++
+        } if (element == 5) {
+            good++
+        }
+
+
+    } if (bad > good) {
+        console.log("2");
+    } if (good > bad) {
+        console.log("5");
+    } if (good == bad) {
+        console.log("=")
+    }
+
 });
