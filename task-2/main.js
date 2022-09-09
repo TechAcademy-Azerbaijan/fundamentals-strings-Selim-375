61cc533fbc200eb721aae178876751f4dc7718dd
Selim Mammadov <mamedovfhn@gmail.com>
2022-09-09 10:52:59 +0400

    task-2

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,16 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+    let a = result.input
+    let b = parseInt(a/100)
+    let c = parseInt(a%10)
+    if (b>c) {
+        console.log(b);
+    } else if (c>b) {
+        console.log(c);
+    } else{
+        console.log("=");
+    }
+
 });
